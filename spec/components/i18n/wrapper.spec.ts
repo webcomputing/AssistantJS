6d9354cd5c15b1cc8d559817c2603e3e55df47e9
JanTrotnow <jtrotnow@web-computing.de>
2018-03-26 15:09:16 +0200

    adds specs for missingInterpolationHandler

--- spec/components/i18n/wrapper.spec.ts
+++ spec/components/i18n/wrapper.spec.ts
@@ -1,5 +1,6 @@
-import { configureI18nLocale } from "../../support/util/i18n-configuration";
 import { arraySplitter } from "../../../src/components/i18n/plugins/array-returns-sample.plugin";
+import { I18nextWrapper } from "../../../src/components/i18n/wrapper";
+import { configureI18nLocale } from "../../support/util/i18n-configuration";
 
 describe("I18nWrapper", function() {
   const expectedTranslations = ["hello my name", "hi my name", "welcome my name"];
@@ -11,16 +12,27 @@ describe("I18nWrapper", function() {
   describe("with returnOnlySample = true", function() {
     describe("translation function", function() {
       it("returns one of many options", function() {
-        let wrapper = this.container.inversifyInstance.get("core:i18n:wrapper");
+        const wrapper = this.container.inversifyInstance.get("core:i18n:wrapper");
         expect(expectedTranslations).toContain(wrapper.instance.t("templateSyntaxSmall", { name: "my name" }))
       })
+    });
+    describe("missingInterpolationHandler", function() {
+      beforeEach(function() {
+        this.wrapper = this.container.inversifyInstance.get("core:i18n:wrapper");
+        spyOn(this.wrapper.instance.options, "missingInterpolationHandler").and.callThrough();
+      });
+
+      it("calls callback if interpolation is missing", function() {
+        this.wrapper.instance.t("templateSyntaxSmall");
+        expect(this.wrapper.instance.options.missingInterpolationHandler).toHaveBeenCalled();
+      });
     });
   });
 
   describe("with returnOnlySample = false", function() {
     describe("translation function", function() {
       it("returns all available options", function() {
-        let wrapper = this.container.inversifyInstance.get("core:i18n:spec-wrapper");
+        const wrapper = this.container.inversifyInstance.get("core:i18n:spec-wrapper");
         expect(wrapper.instance.t("templateSyntaxSmall", { name: "my name" })).toEqual(expectedTranslations.join(arraySplitter));
       })
     });
