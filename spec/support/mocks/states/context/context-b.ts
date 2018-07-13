62335aff1af16a328a3888363a0e65f9a4a5dd5d
Thilo Leise <tl@web-computing.de>
2018-07-13 13:54:30 +0200

    change mocks to fit new responseHandler

--- spec/support/mocks/states/context/context-b.ts
+++ spec/support/mocks/states/context/context-b.ts
@@ -1,26 +1,25 @@
 import { inject, injectable, optional } from "inversify";
+import { BasicHandler } from "../../../../../src/assistant-source";
 import { TranslateHelper } from "../../../../../src/components/i18n/public-interfaces";
 import { Logger } from "../../../../../src/components/root/public-interfaces";
 import { BaseState } from "../../../../../src/components/state-machine/base-state";
 import { stayInContext } from "../../../../../src/components/state-machine/decorators/stay-in-context-decorator";
 import { State, Transitionable } from "../../../../../src/components/state-machine/public-interfaces";
-import { ResponseFactory } from "../../../../../src/components/unifier/public-interfaces";
+import { injectionNames } from "../../../../../src/injection-names";
 
 @stayInContext(() => false)
 @injectable()
 export class ContextBState extends BaseState implements State.Required {
-  public responseFactory: ResponseFactory;
   public extraction: any;
 
   constructor(
-    @inject("core:unifier:current-response-factory") responseFactory: ResponseFactory,
+    @inject(injectionNames.current.responseHandler) responseHandler: BasicHandler<any>,
     @inject("core:unifier:current-extraction") extraction: any,
     @inject("core:i18n:current-translate-helper") translateHelper: TranslateHelper,
     @inject("core:root:current-logger") logger: Logger
   ) {
-    super(responseFactory, translateHelper, extraction, logger);
+    super(responseHandler, translateHelper, extraction, logger);
     this.extraction = extraction;
-    this.responseFactory = responseFactory;
   }
 
   public async exampleBIntent(machine: Transitionable) {
