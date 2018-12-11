918b7e30ca6815884c15cfadaadc14469ce1d3e8
Florian Bachmann <834350+baflo@users.noreply.github.com>
2018-12-11 13:08:55 +0100

    Applies DRY

--- src/components/unifier/entity-dictionary.ts
+++ src/components/unifier/entity-dictionary.ts
@@ -26,21 +26,18 @@ export class EntityDictionary implements EntityDictionaryInterface {
 
       // If not found, try to map synonym to default value
       if (choice === undefined) {
-        const valuesByDistance = customEntities[name]
-          .reduce<Array<{ value: string; distance: number }>>((agg, ch) => {
-            if (!ch.synonyms) {
-              return agg;
-            }
+        // Use Levenshtein distance to find ref value or synonym that's closest to extracted value
+        const allValidValues = ([] as string[]).concat(...customEntities[name].map(ch => (ch.synonyms || []).concat(ch.value)));
+        const closestSynonym = EntityDictionary.getClosestToValue(value, allValidValues);
 
-            const distances = ch.synonyms.map(syn => {
-              return { value: syn, distance: levenshtein.get(syn.toLowerCase(), value.toLowerCase()) };
-            });
+        if (closestSynonym !== undefined) {
+          // Find entity variation that contains the closest synonym
+          const matchedVariation = customEntities[name].find(ch => !!closestSynonym && !!ch.synonyms && ch.synonyms.indexOf(closestSynonym) !== -1);
 
-            return [...agg, ...distances];
-          }, [])
-          .sort((a, b) => a.distance - b.distance);
-
-        value = valuesByDistance[0].value;
+          if (matchedVariation) {
+            value = matchedVariation.value;
+          }
+        }
       }
     }
 
@@ -59,31 +56,12 @@ export class EntityDictionary implements EntityDictionaryInterface {
 
   public getClosest(name: string, validValues: string[], maxDistance?: number) {
     const entityValue = this.get(name);
-    if (typeof entityValue === "undefined") return undefined;
-
-    // Try an exact match for performance reasons
-    const exactMatch = validValues.filter(validValue => validValue.toLowerCase() === entityValue.toLowerCase())[0];
-    if (typeof exactMatch !== "undefined") return exactMatch;
-
-    // Do levenshtein
-    const set = this.getDistanceSet(name, validValues);
-    if (typeof set === "undefined") return undefined;
-
-    // Return element of lowest distance, if greater than
-    let lowestDistance = set[0];
-    set.forEach(entry => {
-      if (entry.distance < lowestDistance.distance) lowestDistance = entry;
-    });
-    return typeof maxDistance === "undefined" || maxDistance >= lowestDistance.distance ? lowestDistance.value : undefined;
+    return EntityDictionary.getClosestToValue(entityValue, validValues, maxDistance);
   }
 
   public getDistanceSet(name: string, validValues: string[]) {
     const entityValue = this.get(name);
-    if (typeof entityValue === "undefined") return undefined;
-
-    return validValues.map(validValue => {
-      return { value: validValue, distance: levenshtein.get(validValue.toLowerCase(), entityValue.toLowerCase()) };
-    });
+    return EntityDictionary.getDistanceToValueSet(entityValue, validValues);
   }
 
   public async storeToSession(session: Session, storeKey = "__currentEntityStore") {
@@ -94,5 +72,32 @@ export class EntityDictionary implements EntityDictionaryInterface {
     const storedData = await session.get(storeKey);
     const storedEntities = typeof storedData === "undefined" ? {} : JSON.parse(storedData);
     this.store = preferCurrentStore ? { ...storedEntities, ...this.store } : { ...this.store, ...storedEntities };
+  }
+
+  private static getClosestToValue(entityValue: string, validValues: string[], maxDistance?: number) {
+    if (typeof entityValue === "undefined") return undefined;
+
+    // Try an exact match for performance reasons
+    const exactMatch = validValues.filter(validValue => validValue.toLowerCase() === entityValue.toLowerCase())[0];
+    if (typeof exactMatch !== "undefined") return exactMatch;
+
+    // Do levenshtein
+    const set = this.getDistanceToValueSet(entityValue, validValues);
+    if (typeof set === "undefined") return undefined;
+
+    // Return element of lowest distance, if greater than
+    let lowestDistance = set[0];
+    set.forEach(entry => {
+      if (entry.distance < lowestDistance.distance) lowestDistance = entry;
+    });
+    return typeof maxDistance === "undefined" || maxDistance >= lowestDistance.distance ? lowestDistance.value : undefined;
+  }
+
+  private static getDistanceToValueSet(entityValue: string, validValues: string[]) {
+    if (typeof entityValue === "undefined") return undefined;
+
+    return validValues.map(validValue => {
+      return { value: validValue, distance: levenshtein.get(validValue.toLowerCase(), entityValue.toLowerCase()) };
+    });
   }
 }
