f76534171515234471b165d95c4d404b31da7516
Dennis Enselmann <de@web-computing.de>
2019-02-15 08:44:59 +0100

    Revert to prepare()-method for compatibility reasons

--- src/spec-helper.ts
+++ src/spec-helper.ts
@@ -98,6 +98,39 @@ export class SpecHelper {
   }
 
   /**
+   * @deprecated since version 0.3.2
+   * Prepares assistant js setup
+   * @param states States to add to container
+   * @param autobind If true, calls setup.autobind()
+   * @param useChilds If set to false, does not set child containers
+   * @param autoSetup If set to true, registers internal components
+   * @param minimumLogLevel If you do not enable logging in specs explicitly using SPEC_LOGS=true, this is the minimum level applied to logger.
+   */
+
+  public prepare(
+    states: State.Constructor[] = [],
+    filters: Array<Constructor<Filter>> = [],
+    autoBind = true,
+    useChilds = false,
+    autoSetup = true,
+    minimumLogLevel: Logger.LogLevel = "warn"
+  ) {
+    // tslint:disable-next-line:no-console
+    console.warn("[DEPRECATION] using prepare() is deprecated. Please use prepareSpec() instead!");
+
+    if (autoSetup) this.assistantJs.registerInternalComponents();
+    if (states.length > 0) this.registerStates(states);
+    if (filters.length > 0) this.registerFilters(filters);
+
+    if (autoBind) this.assistantJs.autobind();
+    if (!useChilds) this.bindChildlessRequestHandlerMock();
+
+    // Change logger unless env variable is set
+    const specLogging = process.env.SPEC_LOGS || process.env.SPECS_LOG || process.env.SPECS_LOGS || process.env.LOG_SPECS;
+    if (!(specLogging === "true")) this.bindSpecLogger(minimumLogLevel);
+  }
+
+  /**
    * Prepares an intent call and creates request scope. Use this first in your specs, then call runMachineAndGetResults() to execute the intent method.
    * @param {PlatformSpecHelper<MergedAnswerTypes, MergedHandler>} platformSpecHelper The platform to use to execute this intent
    * @param {intent} intentToCall The intent you want to execute when calling runMachine() afterwards
